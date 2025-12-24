300a45e07807824abbbfb94c75a2e9911c94a8c5
Hong9915 <hhj4498@naver.com>
2025-12-24 17:14:41 +0900

    fix

--- src/main/java/com/khistory/gateway/GatewayApplication.java
+++ src/main/java/com/khistory/gateway/GatewayApplication.java
@@ -3,7 +3,7 @@ package com.khistory.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.khistory.gateway")
 public class GatewayApplication {
 
 	public static void main(String[] args) {
